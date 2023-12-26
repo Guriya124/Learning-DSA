7b317ed2f857ce2a067eafc27538efcb02e0dea8
Guriya124 <guriyakumarisgvu@gmail.com>
2023-12-26 17:37:09 +0530

    Rotated array by k

--- Array/parctice-Que/arr-que.js
+++ Array/parctice-Que/arr-que.js
@@ -14,4 +14,23 @@ function secondLargest(arr) {
     return secondlargnum;
 }
 
-console.log(secondLargest([12, 35, 1, 10, 34]));
+// console.log(secondLargest([12, 35, 1, 10, 34]));
+
+// ques 2- Rotate array by K
+// Give an integer array nums, rotate the array to the right by k steps,
+// where K is non-negative.
+
+function rotateArray(nums, k) {
+    let size = nums.length;
+    if (size > k) {
+        k = k % size;
+    }
+
+    const rotated = nums.splice(size - k, size);
+    console.log(rotated);
+    nums.unshift(...rotated);
+
+    return nums;
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3));
